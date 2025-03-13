f5e5d765b283fe6b952c9dffd525e14b4cb20d66
sijal1 <muhammadsijal2003@.com>
2025-03-13 16:01:53 -0400

    Initial commit - Django ORM lab setup

---
+++ settings.py
@@ -0,0 +1,17 @@
+# Postgre SQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'WHPLz5oERFyRwkIkbEdCSwGC',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'orm',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
